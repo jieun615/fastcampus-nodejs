bc56c68ee866ce3fea755da6d0dec7b8e2b90d2b
jieun615 <je060105@gmail.com>
2023-12-05 15:39:29 +0900

    Sequelize 설정

---
+++ Sequelize-App/src/models/index.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const dbConfig = {
+    HOST: process.env.INDEX_HOST,
+    USER: process.env.INDEX_USER,
+    PASSWORD: process.env.INDEX_PASSWORD,
+    PORT: process.env.INDEX_PORT,
+    DB: 'postgres',
+    dialect: 'postgres'
+}
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    port: dbConfig.PORT
+})
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+module.exports = db;
